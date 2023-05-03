0e76d625241a5e313aa363b75836cdc788f630f6
Emmanuel Ezema <emmanuelezema6@gmail.com>
2023-05-03 11:29:52 +0100

    new commit

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-const header = document.getElementById('header')
+const headerLg = document.getElementById('header-lg')
+const headerSm = document.getElementById('header-sm')
+const menuBtn = document.querySelector('.menu-btn');
+
 let scrollY = 0;
+
 const handleScroll = () => {
-    header.style.transform = window.scrollY > scrollY ? "translateY(-100%)" : "translateY(0)"
+    headerLg.style.transform = window.scrollY > scrollY ? "translateY(-100%)" : "translateY(0)"
     scrollY = window.scrollY
 }
+
+const handleClick = () => {
+    headerSm.classList.toggle('show-menu')
+}
+
+menuBtn.addEventListener('click', handleClick)
 
 window.addEventListener('scroll', handleScroll)
